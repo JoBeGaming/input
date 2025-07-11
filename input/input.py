6cf9a8132f011e2513f131353f5324e884115c11
JoBe <165585785+JoBeGaming@users.noreply.github.com>
2025-07-11 15:04:30 +0200

    Update input.py, by adding KeyboardStdInObject

--- input/input.py
+++ input/input.py
@@ -6,10 +6,13 @@
 import sys
 import keyboard
 
+from typing import Any
+
 
 
 __all__ = [
-    "input"
+    "input",
+    "KeyboardStdInObject",
 ]
 
 
@@ -37,3 +40,36 @@ def input(msg: str, /, *, file: ... = sys.stdout) -> str:
             file.flush()
           
     return res
+
+class KeyboardStdInObject:
+
+    def __init__(self) -> None:
+        self.res = ""
+        self.__written = False
+
+    def __str__(self) -> str:
+        return self.res
+
+    def __bool__(self) -> bool:
+        return self.__written
+
+    def __add__(self, other: Any) -> str
+        self.__written = True
+        if other.event_type == keyboard.KEY_DOWN:
+            key = other.name
+
+            if key == "enter":
+                self.res += "\n"
+            elif key == "space":
+                self.res += " "
+            elif key == "backspace":
+                if len(self.res) > 0:
+                    self.res = self.res[:-1]
+            elif len(key) == 1:
+                self.res += key
+        return self.res
+
+    def as_file(self) -> TextIO:
+        with open("KeyboardStdInObject.dump", "w") as file:
+            file.write(self.res)
+        return file
